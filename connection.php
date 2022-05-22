deee2f5c0f63ced94368068fb6a18ea4ad94dea8
Ahmad Dannouf <ahmadkdannouf@gmail.com>
2022-05-22 19:13:25 -0400

    Changed connection

--- connection.php
+++ connection.php
@@ -3,10 +3,23 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 
+
+    /*
+    Development connection 
+
     $host = '127.1.0.1';
     $db = 'shopify';
     $user = 'root';
     $pass = '';
+    $charset = 'utf8mb4';
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    */
+
+    //Remote connection
+    $host = 'remotemysql.com';
+    $db = 'EM41McmNcE';
+    $user = 'EM41McmNcE';
+    $pass = 'Vmhy2lZFXG';
     $charset = 'utf8mb4';
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
